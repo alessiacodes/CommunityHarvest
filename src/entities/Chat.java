5a55dd16fbd2226aa8e868e5b69d1d4335c95581
Alessia <alessia.ruberto04@gmail.com>
2024-02-16 23:40:57 -0500

    i forgot getters and setters oopsies

--- src/entities/Chat.java
+++ src/entities/Chat.java
@@ -4,4 +4,28 @@ public class Chat {
     private User user1;
     private User user2;
     private List<String> messages; // Where messages are stored, [0] is earliest message
+
+    public User getUser1() {
+        return user1;
+    }
+
+    public void setUser1(User user1) {
+        this.user1 = user1;
+    }
+
+    public User getUser2() {
+        return user2;
+    }
+
+    public void setUser2(User user2) {
+        this.user2 = user2;
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<String> messages) {
+        this.messages = messages;
+    }
 }
--- src/entities/User.java
+++ src/entities/User.java
@@ -9,4 +9,44 @@ public class User {
     //private String address = None;
     private List<Post> posts;
     private List<Chat> chats;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public List<Chat> getChats() {
+        return chats;
+    }
+
+    public void setChats(List<Chat> chats) {
+        this.chats = chats;
+    }
 }
